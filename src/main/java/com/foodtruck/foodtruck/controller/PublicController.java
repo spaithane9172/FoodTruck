dda2491f5e04271d250b58cac7ba12b6c6a490bb
Sachin P. <spaithane9172@gmail.com>
2024-03-21 19:37:51 +0530

    done

--- src/main/java/com/foodtruck/foodtruck/controller/PublicController.java
+++ src/main/java/com/foodtruck/foodtruck/controller/PublicController.java
@@ -1,30 +1,61 @@
 package com.foodtruck.foodtruck.controller;
 
+import java.util.Set;
+
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/public")
 public class PublicController {
+    static final String PATH = "redirect:/public/";
 
     @RequestMapping("/")
-    public String home() {
+    public String home(Model m) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Set<String> role = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+        m.addAttribute("isUser", role.contains("ROLE_USER"));
+        m.addAttribute("isFoodtruck", role.contains("ROLE_FOODTRUCK"));
+
+        if (authentication == null || authentication instanceof AnonymousAuthenticationToken)
+            m.addAttribute("isUserLogged", false);
+        else
+            m.addAttribute("isUserLogged", true);
+
         return "index";
     }
 
     @RequestMapping("/registerFoodTruck")
     public String registerFoodTruck() {
-        return "registerFoodTruck";
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication instanceof AnonymousAuthenticationToken)
+            return "registerFoodTruck";
+        else
+            return PATH;
     }
 
     @RequestMapping("/loginPage")
     public String loginPage() {
-        return "loginPage";
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication instanceof AnonymousAuthenticationToken)
+            return "loginPage";
+        else
+            return PATH;
+
     }
 
     @RequestMapping("/registerUser")
     public String registerUser() {
-        return "registerUser";
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || authentication instanceof AnonymousAuthenticationToken)
+            return "registerUser";
+        else
+            return PATH;
     }
 
 }
